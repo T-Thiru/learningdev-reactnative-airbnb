f8d45c1552d7e6d004eafeb32581038d1743bbe1
Thiruvaran Thayalan <110049957+T-Thiru@users.noreply.github.com>
2022-11-22 14:51:56 +0100

    initial commit

---
+++ containers/SignUpScreen.js
@@ -0,0 +1,25 @@
+import { Button, Text, TextInput, View } from "react-native";
+
+export default function SignUpScreen({ setToken }) {
+  return (
+    <View>
+      <View>
+        <Text>Name: </Text>
+        <TextInput placeholder="Username" />
+        <Text>Email: </Text>
+        <TextInput placeholder="Email" />
+        <Text>Password: </Text>
+        <TextInput placeholder="Password" secureTextEntry={true} />
+        <Text>Confirm Password: </Text>
+        <TextInput placeholder="Password" secureTextEntry={true} />
+        <Button
+          title="Sign up"
+          onPress={async () => {
+            const userToken = "secret-token";
+            setToken(userToken);
+          }}
+        />
+      </View>
+    </View>
+  );
+}
